32737b74ee36b5b635ee1eafe09ef3e906b686b0
Paulo Barcelos <paulo@azucrina.org>
2015-10-29 16:36:18 +0100

    / updated publish script

--- s3-publish.sh
+++ s3-publish.sh
@@ -1,6 +1,11 @@
 rm -r s3_publish
 mkdir s3_publish
 
-cp Quirkbot-Windows-Drivers-Installer.exe s3_publish/quirkbot-drivers.exe
+cp "Quirkbot-Windows-Drivers-Installer.exe" "s3_publish/quirkbot-windows-drivers.exe"
+aws s3 sync s3_publish s3://code.quirkbot.com/downloads --exclude *.DS_Store
 
+rm -r s3_publish
+mkdir s3_publish
+
+cp Quirkbot-Windows-Drivers-Installer.exe s3_publish/quirkbot-drivers.exe
 aws s3 sync s3_publish s3://code.quirkbot.com/install/windows --delete --exclude *.DS_Store
